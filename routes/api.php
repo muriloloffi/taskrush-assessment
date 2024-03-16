e2b3f59dea57712a194d1af674d05dbce1310c9e
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-03-16 09:58:56 -0300

    Fix typo in project route path

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 // Get all projects
 Route::get('/projects', [ProjectController::class, 'index']);
 
-Route::get('/projects/{id}', [ProjectController::class, 'show']);
+Route::get('/project/{id}', [ProjectController::class, 'show']);
 
 // Start a new workInterval for a given user and task
 Route::post('/work-intervals', [WorkIntervalController::class, 'start']);
