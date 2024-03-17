727af9825673d871503f8f20b00cd3357d14764c
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-03-17 16:16:41 -0300

    Eager load the relationships of a project

--- app/Http/Controllers/Api/v1/ProjectController.php
+++ app/Http/Controllers/Api/v1/ProjectController.php
@@ -19,7 +19,7 @@ class ProjectController extends Controller
     #[UrlParam(name: 'id', type: 'integer', description: 'The ID of the project.', example: 1)]
     public function show(int $id)
     {
-        $project = Project::with(['tasks', 'tasks.workIntervals'])->find($id);
+        $project = Project::with(['owner', 'members', 'tasks', 'tasks.workIntervals', 'tasks.owner'])->find($id);
 
         if (!$project) {
             return response()->json(['message' => 'Project not found'], 404);
