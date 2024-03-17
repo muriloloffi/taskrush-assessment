2970a2c86bcfd7e3b8897e7f032bea698167ce38
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-03-17 16:15:51 -0300

    Fix Project members relationship

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
@@ -26,9 +27,9 @@ class Project extends Model
         return $this->belongsTo(User::class, 'owner_id');
     }
 
-    public function members(): HasMany
+    public function members(): BelongsToMany
     {
-        return $this->hasMany(User::class, 'projects_members');
+        return $this->belongsToMany(User::class, 'projects_members');
     }
 
     public function tasks(): HasMany
